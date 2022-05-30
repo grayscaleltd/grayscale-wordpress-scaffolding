c222ac749be7f22bae4dd66e060b952677447b26
K Chan <k.chan@grayscale.com.hk>
2022-05-30 16:28:43 +0800

    chore: update package dependencies and code style

--- src/templates/functions-gutenberg.php
+++ src/templates/functions-gutenberg.php
@@ -52,3 +52,4 @@
   // ==========================================================================
   remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
   remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+

42c5ea689f0b0acdcc3dc568d13b24e94056dd11
K Chan <k.chan@grayscale.com.hk>
2024-08-05 06:08:48 +0800

    feat: reduce normalize.css specificity with @layer (#44)
    * chore: update stylelint rules
    
    * chore: update composer dependencies
    
    * chore: update gulp setup
    
    * feat: reduce normalize.css specificity with @layer
    
    * chore: update package dependencies

--- tasks/setup.js
+++ tasks/setup.js
@@ -1,12 +1,12 @@
 import config from '../gulpconfig.js';
 
+import {checkbox, input} from '@inquirer/prompts';
 import chalk from 'chalk';
 import {deleteAsync} from 'del';
 import download from 'download';
 import fs from 'node:fs';
 import gulp from 'gulp';
 import gulpStringReplace from 'gulp-string-replace';
-import inquirer from 'inquirer';
 import nodeFetch from 'node-fetch';
 import ora from 'ora';
 import through from 'through2';
@@ -17,12 +17,9 @@ const gulpStringReplaceOption = {
   },
 };
 
-let projectName = '';
-let projectSlug = '';
-let projectURL = '';
-let projectSalt = '';
+const project = {};
 
-function setProjectIdentifiers() {
+async function prepareSetup() {
   process.stdout.write(
       process.platform === 'win32' ?
         '\x1B[2J\x1B[0f' :
@@ -35,143 +32,130 @@ function setProjectIdentifiers() {
       '\n',
   );
 
-  return inquirer.prompt([
-    {
-      name: 'projectName',
-      message: 'Project name (alphabets, hyphens, and spaces only):',
-      validate: (value) => {
-        if (value === '') {
-          return 'Project name cannot be empty!';
-        } else if (/^[ ]/.test(value)) {
-          return 'Project name cannot start with a space!';
-        } else if (/[^A-Za-z -]/.test(value)) {
-          return 'Alphabets, hyphens, and spaces only!';
-        } else {
-          projectName = value;
-          return true;
-        }
-      },
-    },
-    {
-      name: 'projectSlug',
-      message: 'Project slug (alphabets and underscores only):',
-      default: () => {
-        return projectName.replace(/[^0-9|A-Z|a-z]/g, '_')
-            .replace(/_+/g, '_')
-            .toLowerCase();
-      },
-      validate: (value) => {
-        if (value === '') {
-          return 'Project slug cannot be empty!';
-        } else if (/[^A-Za-z_]/.test(value)) {
-          return 'Alphabets and underscores only!';
-        } else {
-          projectSlug = value;
-          return true;
-        }
-      },
-    },
-    {
-      name: 'projectURL',
-      message: 'Project URL:',
-      default: () => {
-        return 'https://' + projectSlug + '.com';
-      },
-      validate: (value) => {
-        if (/[^A-Za-z:/\-.]/.test(value)) {
-          return 'Alphabets, colon, forward slashes, and hyphens only!';
-        } else {
-          projectURL = value;
-          return true;
-        }
-      },
-    },
-  ]).then((answers) => {
-    return gulp.src([
-      './src/**',
-      './gulpconfig.js',
-      './package.json',
-    ], {base: './'})
-        .pipe(gulpStringReplace(
-            /: Grayscale/g,
-            `: ${projectName}`,
-            gulpStringReplaceOption
-        ))
-        .pipe(gulpStringReplace(
-            /Theme tailor-made for Grayscale/g,
-            `Theme tailor-made for ${projectName}`,
-            gulpStringReplaceOption
-        ))
-        .pipe(gulpStringReplace(
-            /: grayscale/g,
-            `: ${projectSlug}`,
-            gulpStringReplaceOption
-        ))
-        .pipe(gulpStringReplace(
-            /'grayscale'/g,
-            `'${projectSlug}'`,
-            gulpStringReplaceOption
-        ))
-        .pipe(gulpStringReplace(
-            /grayscale-wordpress-scaffolding/g,
-            `${projectSlug}`,
-            gulpStringReplaceOption
-        ))
-        .pipe(gulpStringReplace(
-            /"version": "(\d|\.)+"/g,
-            `"version": "0.0.1"`,
-            gulpStringReplaceOption
-        ))
-        .pipe(gulpStringReplace(
-            /"homepage": "https:\/\/grayscale\.com\.hk"/g,
-            `"homepage": "${projectURL}"`,
-            gulpStringReplaceOption
-        ))
-        .pipe(gulp.dest('./'));
-  });
-}
-
-function downloadDepdencies(cb) {
-  const dependencyQuestions = [];
-
-  Object.keys(config.setup.dep).forEach((depName) => {
-    dependencyQuestions.push({
-      name: depName,
-      message: `Should I download ${depName}?`,
-      type: 'confirm',
-      default: false,
-    });
-  });
-
-  inquirer.prompt(dependencyQuestions).then((answers) => {
-    const dependencySrcs = [];
-
-    Object.keys(config.setup.dep).forEach((depName) => {
-      if (answers[depName]) {
-        dependencySrcs.push(config.setup.dep[depName]);
+  project.name = await input({
+    message: 'Project name (alphabets, hyphens, and spaces only):',
+    required: true,
+    validate: (value) => {
+      if (/^[ ]/.test(value)) {
+        return 'Project name cannot start with a space!';
+      } else if (/[^A-Za-z -]/.test(value)) {
+        return 'Alphabets, hyphens, and spaces only!';
       }
-    });
 
-    if (!dependencySrcs) {
-      cb();
-    } else {
-      const spinner = ora('Downloading...').start();
+      return true;
+    },
+  });
 
-      Promise.all(dependencySrcs.map((url) => download(url, config.setup.dest, {
+  project.slug = await input({
+    message: 'Project slug (alphabets and underscores only):',
+    default: () => {
+      return project.name.replace(/[^0-9|A-Z|a-z]/g, '_')
+          .replace(/_+/g, '_')
+          .toLowerCase();
+    },
+    required: true,
+    validate: (value) => {
+      if (/[^A-Za-z_]/.test(value)) {
+        return 'Alphabets and underscores only!';
+      }
+
+      return true;
+    },
+  });
+
+  project.url = await input({
+    message: 'Project URL:',
+    default: () => {
+      return 'https://' + project.slug + '.com';
+    },
+    required: true,
+    validate: (value) => {
+      if (/[^A-Za-z:/\-.]/.test(value)) {
+        return 'Alphabets, colon, forward slashes, and hyphens only!';
+      }
+
+      return true;
+    },
+  });
+
+  project.dependencies = [];
+
+  Object.keys(config.setup.dep).map((key) => {
+    project.dependencies = [
+      ...project.dependencies,
+      {
+        name: key,
+        value: config.setup.dep[key],
+      }
+    ];
+  });
+
+  project.dependencies = await checkbox({
+    message: `Should I download any of the following?`,
+    choices: project.dependencies,
+  });
+
+  if (!project.dependencies.length) {
+    console.log(chalk.cyan('No'));
+  } else {
+    const spinner = ora('Downloading...').start();
+
+    await Promise.all(project.dependencies.map((url) => {
+      return download(url, config.setup.dest, {
         extract: true,
         strip: 1,
-      }))).then((resolved) => {
-        spinner.succeed(`Downloaded ${resolved.length} item(s).`);
-      }).catch((error) => {
-        spinner.fail(`Download incomplete: ${error.name}.`);
-      }).finally(() => {
-        cb();
       });
-    }
-  });
+    })).then((resolved) => {
+      spinner.succeed(`Downloaded ${resolved.length} item(s).`);
+    }).catch((error) => {
+      spinner.fail(`Download incomplete: ${error.name}.`);
+    });
+  }
+
+  return gulp.src([
+    './src/**',
+    './gulpconfig.js',
+    './package.json',
+  ], {base: './'})
+      .pipe(gulpStringReplace(
+          /: Grayscale/g,
+          `: ${project.name}`,
+          gulpStringReplaceOption
+      ))
+      .pipe(gulpStringReplace(
+          /Theme tailor-made for Grayscale/g,
+          `Theme tailor-made for ${project.name}`,
+          gulpStringReplaceOption
+      ))
+      .pipe(gulpStringReplace(
+          /: grayscale/g,
+          `: ${project.slug}`,
+          gulpStringReplaceOption
+      ))
+      .pipe(gulpStringReplace(
+          /'grayscale'/g,
+          `'${project.slug}'`,
+          gulpStringReplaceOption
+      ))
+      .pipe(gulpStringReplace(
+          /grayscale-wordpress-scaffolding/g,
+          `${project.slug}`,
+          gulpStringReplaceOption
+      ))
+      .pipe(gulpStringReplace(
+          /"version": "(\d|\.)+"/g,
+          `"version": "0.0.1"`,
+          gulpStringReplaceOption
+      ))
+      .pipe(gulpStringReplace(
+          /"homepage": "https:\/\/grayscale\.com\.hk"/g,
+          `"homepage": "${project.url}"`,
+          gulpStringReplaceOption
+      ))
+      .pipe(gulp.dest('./'));
 }
 
-function wpRemoveThemes() {
+function removeWPthemes() {
   return deleteAsync([
     config.setup.dest + '/wp-content/themes/**',
     '!' + config.setup.dest + '/wp-content/themes',
@@ -189,25 +173,25 @@ function fetchWPsalt() {
         }
       })
       .then((salt) => {
-        projectSalt = salt.split('\n').filter((line) => (line)).join('\n');
+        project.salt = salt.split('\n').filter((line) => (line)).join('\n');
       })
       .catch((error) => {
         console.error(chalk.red('fetchWPsalt Error: ' + error.message));
       });
 }
 
-function wpReplaceSalt() {
+function replaceWPsalt() {
   return gulp.src(config.setup.dest + '/wp-config*.php')
       .pipe(through.obj(function(file, enc, cb) {
         const oldWPconfig = file.contents.toString(enc).split(/\r\n|\r|\n/g);
         let newWPconfig = [];
         let isSaltReplaced = false;
 
-        oldWPconfig.forEach((line, index) => {
+        oldWPconfig.forEach((line) => {
           if (!line.match('put your unique phrase here')) {
             newWPconfig.push(line);
           } else if (!isSaltReplaced) {
-            newWPconfig.push(projectSalt);
+            newWPconfig.push(project.salt);
             isSaltReplaced = true;
           }
         });
@@ -246,10 +230,9 @@ function displayCompleteHint(cb) {
 }
 
 export default gulp.series(
-    setProjectIdentifiers,
-    downloadDepdencies,
-    wpRemoveThemes,
+    prepareSetup,
+    removeWPthemes,
     fetchWPsalt,
-    wpReplaceSalt,
+    replaceWPsalt,
     displayCompleteHint,
 );

1d7a2144a8098aa76a024dc7319eca44d6d110c5
K Chan <k.chan@grayscale.com.hk>
2025-05-14 01:13:37 +0800

    fix: add i18n to number of comments

--- src/templates/archive.php
+++ src/templates/archive.php
@@ -52,7 +52,13 @@
 							<?php if (comments_open() || get_comments_number()) : ?>
 								<li>
 									<a href="<?php comments_link(); ?>">
-										<?php comments_number('No Comments', '1 Comment', '% Comments'); ?>
+										<?php
+											comments_number(
+												__('No Comments', 'grayscale'),
+												__('1 Comment', 'grayscale'),
+												__('% Comments', 'grayscale')
+											);
+										?>
 									</a>
 								</li>
 							<?php endif; ?>
